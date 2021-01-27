5a9f2e473a5bece111a3aec4c0d787dae7e427b5
Emanuel Honorio <emanuelh.dev@gmail.com>
2021-01-27 00:20:41 -0300

    fix deploy

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/pog-ecommerce-ui"));
+app.use(express.static(__dirname + "/dist/ecommerce-ui"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(__dirname + "/dist/pog-ecommerce-ui/index.html");
+  res.sendFile(__dirname + "/dist/ecommerce-ui/index.html");
 });
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 4200, () => {
+  console.log("app is running at port " + 4200);
+});
