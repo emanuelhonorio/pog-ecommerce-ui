ddd4f35b84275faa29698493f34d21d4ef3c7c8f
Emanuel Honorio <emanuelh.dev@gmail.com>
2021-02-12 22:51:03 -0300

    add stripePublishableKey variable  to enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   baseApiUrl: 'https://pog-ecommerce-api.herokuapp.com',
   LSTokenKey: '#$p$$__',
+  stripePublishableKey:
+    'pk_test_51IGy5UFswIj4Je5zI4lgYG1AvNwCnQdBl5oh1KLygjHs0fQaMdkVrAcn5rcqwXP2eAMJM0j3mjVxJYg5jBPKhOkN00TwnOS96u',
 };
